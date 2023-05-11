87b2cc0363fc293d161552f687744a20fc57b91a
Nguyen Hoang Kiet <skill1sp2@gmail.com>
2023-05-12 04:30:54 +0700

    Add Get My Booking

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -4,4 +4,5 @@ const bookingController = require("../controllers/booking.js");
 
 router.post("/createBooking/:userId", bookingController.createBooking);
 router.get("/getAllListBooking", bookingController.getAllListBooking);
+router.get("/getMyBooking/:userId", bookingController.getMyBookings);
 module.exports = router;
