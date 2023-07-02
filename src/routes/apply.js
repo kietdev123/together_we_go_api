865a57773e11f3be1376cfebbe2f03ba9d7676b6
Quan Le <quanxyzabc@gmail.com>
2023-07-02 21:09:31 +0700

    Add Get Booking in Apply

--- src/routes/apply.js
+++ src/routes/apply.js
@@ -6,5 +6,5 @@ router.post("/", applyController.createApply);
 router.patch("/:id", applyController.updateApply);
 router.get("/getMyApply/:userId", applyController.getMyApply);
 router.get("/getApplyBooking/:bookingId", applyController.getApplyBooking);
-
+router.get("/getBookingInApply/:applyId", applyController.getBookingInApply);
 module.exports = router;
