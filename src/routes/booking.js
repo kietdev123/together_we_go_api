5c3c9a7efe070cf95d90eb03d1219a9e5bd9aac1
Quan Le <quanxyzabc@gmail.com>
2023-07-07 02:20:04 +0700

    Add get list complete booking by userId

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -5,4 +5,8 @@ const bookingController = require("../controllers/booking.js");
 router.post("/createBooking/:userId", bookingController.createBooking);
 router.get("/getAllListBooking", bookingController.getAllListBooking);
 router.get("/getMyBooking/:userId", bookingController.getMyBookings);
+router.get(
+  "/getMyCompleteBooking/:userId",
+  bookingController.getMyCompleteBooking
+);
 module.exports = router;
