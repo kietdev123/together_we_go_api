0288669b5c2e9d4d227d4c30785cd6b54cf9d097
kiet <79886528+kiet-kun@users.noreply.github.com>
2023-09-08 13:31:24 +0700

    Add user swagger

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,12 +2,8 @@ const { Router } = require("express");
 const userController = require("../controllers/user");
 const router = Router();
 
-router.post("/edit_avatar/:userId", userController.editAvatar);
-router.get("/get_user/:userId", userController.getUser);
-router.patch("/update_user/:userId", userController.updateUser);
-router.patch(
-  "/update_user_location/:userId",
-  userController.updateUserLocation
-);
+router.get("/", userController.get);
+router.patch("/", userController.update);
 
 module.exports = router;
+
