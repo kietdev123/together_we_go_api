2e7e6b763c61e8722fb7b556ac4d27219854ade6
kiet <79886528+kiet-kun@users.noreply.github.com>
2023-09-08 12:26:03 +0700

    Add chat room swagger

--- src/controllers/chat_room.js
+++ src/controllers/chat_room.js
@@ -1,231 +1,114 @@
 const mongoose = require("mongoose");
-const ChatRoom = require("../models/chat_room");
-const User = require("../models/user");
-const chat_room = require("../models/chat_room");
+const ChatRoom = require("../models/chat_room.js");
 const { sendSuccess, sendError, sendServerError} = require("../utils/client.js");
+const message_name = "chat_room";
 
-exports.createChatRoom = (req, res, next) => {
-  const chat_room = new ChatRoom({
-    userId_1: new mongoose.Types.ObjectId(req.body.userId_1),
-    userId_2: new mongoose.Types.ObjectId(req.body.userId_2),
-    user1: new mongoose.Types.ObjectId(req.body.userId_1),
-    user2: new mongoose.Types.ObjectId(req.body.userId_2),
-  });
-  chat_room
-    .save()
-    .then((result) => {
-      res.status(201).json({
-        message: "Chat room added succesfully",
-        post: {
-          ...result,
-          id: result._id,
-        },
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: "Fail to create chat room!",
-      });
+exports.create = async (req, res, next) => {
+  try {
+    let data = new ChatRoom({
+      userId_1: new mongoose.Types.ObjectId(req.body.userId_1),
+      userId_2: new mongoose.Types.ObjectId(req.body.userId_2),
+      user1: new mongoose.Types.ObjectId(req.body.userId_1),
+      user2: new mongoose.Types.ObjectId(req.body.userId_2),
     });
+    await data.save();
+    return sendSuccess(res, `${message_name} added succesfully`, data);
+  }
+  catch (err) {
+    console.log(error);
+    return sendServerError(res);
+  }
+ 
 };
 
-exports.getAllChatRoomWithUserId = async (req, res) => {
+exports.getList = async (req, res, next) => {
   try {
-    const userId = req.params.userId;
-    console.log(userId);
+    let filter = {};
+    let {page, pageSize, sortCreatedAt, sortUpdatedAt, user1_id, user2_id} = req.query;
+    let skipNum = 0;
 
-    // let chatRoom = await ChatRoom.find({ userId_1: userId });
-    // console.log(chatRoom);
+    if (page) page = Number(page);
+    else page = 1
 
-    // const result = await ChatRoom.aggregate([
-    //   {
-    //     $match: {
-    //       $or: [
-    //         { userId_1: new mongoose.Types.ObjectId(userId) },
-    //         { userId_2: new mongoose.Types.ObjectId(userId) },
-    //       ],
-    //     },
-    //   },
-    //   {
-    //     $lookup: {
-    //       from: "users",
-    //       localField: "userId_1",
-    //       foreignField: "_id",
-    //       as: "user1",
-    //     },
-    //   },
-    //   {
-    //     $lookup: {
-    //       from: "users",
-    //       localField: "userId_2",
-    //       foreignField: "_id",
-    //       as: "user2",
-    //     },
-    //   },
-    //   {
-    //     $project: {
-    //       _id: 1, // "1" means on
-    //       userId_1: 1,
-    //       userId_2: 1,
-    //       user1: 1, // "0" means off
-    //       user2: 1,
-    //     },
-    //   },
-    // ]);
+    if (pageSize) pageSize = Number(pageSize);
+    else pageSize = 20;
 
-    // console.log(result);
+    skipNum = (page - 1) * pageSize;
+    if (skipNum < 0) skipNum = 0;
 
-    let result = await ChatRoom.find({
-      $or: [
-        { userId_1: new mongoose.Types.ObjectId(userId) },
-        { userId_2: new mongoose.Types.ObjectId(userId) },
-      ],
-    })
-      .populate("user1")
-      .populate("user2")
-      .populate("lastMessage");
-
-    // console.log(result);
-    let customChatRoom = [];
-
-    for (let i = 0; i < result.length; i++) {
-      let newChatRoom = result[i];
-
-      let finalChatRoom = {};
-      if (newChatRoom.userId_1 == userId) {
-        finalChatRoom = {
-          id: newChatRoom._id,
-          partner_name: newChatRoom.user2.first_name,
-          partner_gmail: newChatRoom.user2.email,
-          partner_avatar: newChatRoom.user2.avatarUrl,
-          partner_id: newChatRoom.user2._id,
-          num_unwatch: newChatRoom.num_unwatched_1,
-          lastMessage: newChatRoom.lastMessage,
-        };
-      } else {
-        finalChatRoom = {
-          id: newChatRoom._id,
-          partner_name: newChatRoom.user1.first_name,
-          partner_gmail: newChatRoom.user1.email,
-          partner_avatar: newChatRoom.user1.avatarUrl,
-          partner_id: newChatRoom.user1._id,
-          num_unwatch: newChatRoom.num_unwatched_2,
-          lastMessage: newChatRoom.lastMessage,
-        };
+    if (user1_id && !user2_id){
+      filter = {
+        $or: [
+          {
+            userId_1: new mongoose.Types.ObjectId(user1_id),
+          },
+          {
+            userId_2: new mongoose.Types.ObjectId(user1_id),
+          },
+        ],
       }
-      if (finalChatRoom.lastMessage == undefined) {
-        finalChatRoom.lastMessage = {
-          message: "Hộp thoại trống",
-          type: "isText",
-          createdAt: new Date().toISOString(),
-        };
-      }
-      result[i] = finalChatRoom;
-      customChatRoom.push(finalChatRoom);
     }
-    console.log(result);
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
+    if (!user1_id && user2_id){
+      filter = {
+        $or: [
+          {
+            userId_1: new mongoose.Types.ObjectId(user2_id),
+          },
+          {
+            userId_2: new mongoose.Types.ObjectId(user2_id),
+          },
+        ],
+      }
+    }
+    if (user1_id && user2_id){
+      filter = {
+        $or: [
+          {
+            userId_1: new mongoose.Types.ObjectId(user1_id),
+            userId_2: new mongoose.Types.ObjectId(user2_id),
+          },
+          {
+            userId_2: new mongoose.Types.ObjectId(user1_id),
+            userId_1: new mongoose.Types.ObjectId(user2_id),
+          },
+        ],
+      }
+    }
+
+    let _sort = {};
+    if (sortCreatedAt) _sort.createdAt = Number(sortCreatedAt);
+    if (sortUpdatedAt) _sort.updatedAt = Number(sortUpdatedAt);
+
+    const datas = await ChatRoom
+    .find(filter)
+    .sort(_sort)
+    .skip(skipNum)
+    .limit(pageSize)
+    .populate("user1")
+    .populate("user2")
+    .populate("lastMessage");
+    
+    return sendSuccess(res,`Get ${message_name} succesfully`, datas, datas.length);
+
+  } catch (e) {
+    console.log(e);
+    return sendServerError(res);
   }
 };
-//End region
 
 exports.getOne = async (req, res) => {
   try {
-    let chatRoom = await ChatRoom.findById(req.params.chatRoomId)
+    let data = await ChatRoom.findById(req.params.id)
       // .populate("user1", { id: 1, first_name: 1 })
       // .populate("user2", { id: 1, first_name: 1 });
       .populate("user1")
       .populate("user2")
       .populate("lastMessage");
-    res.status(200).json(chatRoom);
+    
+    return sendSuccess(res, `Get 1 ${message_name} successfully`, data);
   } catch (e) {
-    res.status(400).json({ message: e.message });
+    console.log(e);
+    return sendServerError(res);
   }
 };
 
-exports.getChatRoomInBooking = async (req, res) => {
-  try {
-    console.log(req.params);
-    let chatRoom = await ChatRoom.findOne({
-      $or: [
-        { userId_1: req.params.posterId, userId_2: req.params.current_user_id },
-        { userId_2: req.params.posterId, userId_1: req.params.current_user_id },
-      ],
-    })
-      // .populate("user1", { id: 1, first_name: 1 })
-      // .populate("user2", { id: 1, first_name: 1 });
-      .populate("user1")
-      .populate("user2")
-      .populate("lastMessage");
-
-    console.log("kiet ", chatRoom);
-
-    if (chatRoom == null) {
-      console.log("heelo");
-      chatRoom = new ChatRoom({
-        userId_1: new mongoose.Types.ObjectId(req.params.posterId),
-        userId_2: new mongoose.Types.ObjectId(req.params.current_user_id),
-        user1: new mongoose.Types.ObjectId(req.params.posterId),
-        user2: new mongoose.Types.ObjectId(req.params.current_user_id),
-      });
-      await chatRoom.save();
-
-      chatRoom = await ChatRoom.findOne({
-        $or: [
-          {
-            userId_1: req.params.posterId,
-            userId_2: req.params.current_user_id,
-          },
-          {
-            userId_2: req.params.posterId,
-            userId_1: req.params.current_user_id,
-          },
-        ],
-      })
-        // .populate("user1", { id: 1, first_name: 1 })
-        // .populate("user2", { id: 1, first_name: 1 });
-        .populate("user1")
-        .populate("user2")
-        .populate("lastMessage");
-    }
-    let newChatRoom = chatRoom;
-
-    let finalChatRoom = {};
-
-    if (newChatRoom.user2._id == req.params.posterId) {
-      finalChatRoom = {
-        id: newChatRoom._id,
-        partner_name: newChatRoom.user2.first_name,
-        partner_gmail: newChatRoom.user2.email,
-        partner_avatar: newChatRoom.user2.avatarUrl,
-        partner_id: newChatRoom.user2._id,
-        num_unwatch: newChatRoom.num_unwatched_1,
-        lastMessage: newChatRoom.lastMessage,
-      };
-    } else {
-      finalChatRoom = {
-        id: newChatRoom._id,
-        partner_name: newChatRoom.user1.first_name,
-        partner_gmail: newChatRoom.user1.email,
-        partner_avatar: newChatRoom.user1.avatarUrl,
-        partner_id: newChatRoom.user1._id,
-        num_unwatch: newChatRoom.num_unwatched_1,
-        lastMessage: newChatRoom.lastMessage,
-      };
-    }
-
-    if (finalChatRoom.lastMessage == undefined) {
-      finalChatRoom.lastMessage = {
-        message: "Hộp thoại trống",
-        type: "isText",
-        createdAt: new Date().toISOString(),
-      };
-    }
-
-    res.status(200).json(finalChatRoom);
-  } catch (e) {
-    res.status(400).json({ message: e.message });
-  }
-};
