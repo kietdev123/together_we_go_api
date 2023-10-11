b590d954acaf01bea05cd46a142cc0314b230a9b
kiet <79886528+kiet-kun@users.noreply.github.com>
2023-10-11 07:48:27 +0700

    add crud user

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,15 @@ const { Router } = require("express");
 const userController = require("../controllers/user");
 const router = Router();
 
-router.get("/", userController.get);
-router.patch("/", userController.update);
+router.get("/profile", userController.getInfo);
+router.patch("/profile", userController.updateInfo);
+
+// router.post("/", userController.create);
+
+router.patch("/:id", userController.update);
+router.get("/:id", userController.getOne);
+router.get("/", userController.getList);
+router.delete("/:id", userController.delete);
 
 module.exports = router;
 
