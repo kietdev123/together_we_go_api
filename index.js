e9bad6151475efa65dae4409831c71d2640ea8fd
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-03-13 09:37:22 +0700

    fix: move index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const app = require("./app");
+const app = require("./src/app");
 const server = http.createServer(app);
 
 const { API_PORT } = process.env;
@@ -17,7 +17,7 @@ let io = require("socket.io")(server, {
   },
 });
 exports.io = io;
-require("./sockets/socket");
-require("./sockets/features/chat.feature");
-require("./controllers/message");
+require("./src/sockets/socket");
+require("./src/sockets/features/chat.feature");
+require("./src/controllers/message");
 
