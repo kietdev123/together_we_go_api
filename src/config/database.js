cef8c412aa917e355a5bc2ac4daab7c3395e0625
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-03-13 13:26:37 +0700

    chore: change db var name

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const { MONGO_URI } = process.env;
+const { MONGODB_URI } = process.env;
 
 exports.connect = () => {
   // Connecting to the database
   mongoose
-    .connect(MONGO_URI, {
+    .connect(MONGODB_URI, {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
       // useCreateIndex: true,
